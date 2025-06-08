460924c94cbe536727bd08665911cbf8421adffb
Hafsa Feroz <hferoz1@depaul.edu>
2025-06-08 12:03:47 -0500

    SE350 text drawing

---
+++ src/main/java/org/example/TextSE350Command.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class TextSE350Command extends CompositeCommand {
+
+    public TextSE350Command(Turtle turtle, Matrix matrix, double size) {
+        super();
+        double spacing = size + 2;
+        addCommand(new LetterSCommand(turtle, matrix, size));
+        addCommand(new MoveCommand(turtle, matrix, spacing));
+        addCommand(new LetterECommand(turtle, matrix, size));
+        addCommand(new MoveCommand(turtle, matrix, spacing * 1.5));
+        addCommand(new Digit3Command(turtle, matrix, size));
+        addCommand(new MoveCommand(turtle, matrix, spacing));
+        addCommand(new LetterSCommand(turtle, matrix, size));
+        addCommand(new MoveCommand(turtle, matrix, spacing));
+        addCommand(new RectangleCommand(turtle, matrix, size, size));
+    }
+}
